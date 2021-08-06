09616ca3e12c84fcb7e747c262cf712526bd9cba
Alex Mai <alexandertmai@gmail.com>
2021-08-05 17:15:41 -0700

    Added alert wildfire dataset

--- beit/semantic_segmentation/configs/_base_/datasets/alertwildfire_640x640.py
+++ beit/semantic_segmentation/configs/_base_/datasets/alertwildfire_640x640.py
@@ -1,6 +1,6 @@
 # dataset settings
-dataset_type = 'ADE20KDataset'
-data_root = '/optane/ade/ADEChallengeData2016'
+dataset_type = 'AlertWildfire'
+data_root = '/optane/alertwildfire'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (640, 640)
@@ -52,3 +52,4 @@ data = dict(
         img_dir='images/validation',
         ann_dir='annotations/validation',
         pipeline=test_pipeline))
+
